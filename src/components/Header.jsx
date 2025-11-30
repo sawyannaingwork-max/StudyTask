741d00109da1c42d75484fd2cf47ab2daa15c710
sawyannaingwork-max <sawyannaingwork@gmail.com>
2025-11-30 17:50:11 +0700

    fixing responsive style issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ export default function Header()
 {
     return(
         <header className="bg-[#141617] shadow-sm px-5 py-5 shadow-white flex justify-between items-center">
-            <h1 className="text-3xl text-white">Study Tasks</h1>
+            <h1 className="text-xl sm:text-3xl text-white">Study Tasks</h1>
             <nav>
-                <ul className="flex gap-5">
+                <ul className="flex gap-2 sm:gap-5">
                     <li className="text-sky-300"><NavLink to="/">Home</NavLink></li>
                     <li className="text-sky-300"><NavLink to="/add">Add Task</NavLink></li>
                     <li className="text-sky-300"><NavLink to="/summary">Summary</NavLink></li>
